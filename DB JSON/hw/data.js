fecf82e907c6ee692544b68106c3604b6d2f4d96
harshalthakre123 <harshalthakre.csaiml.oist@gmail.com>
2025-01-18 18:36:31 +0530

    updates

--- DB JSON/hw/data.js
+++ DB JSON/hw/data.js
@@ -6,4 +6,37 @@
         let res= await fetch(url,{method: "GET"})
         let data=await res.json()
         console.log(data);
+    
+    
+        // CRUD read
+        let show=document.querySelector('#displayy')
+
+        // data.map is high order function and internal function is call back function
+        
+        data.map((e)=>{
+
+            show.innerHTML+=`
+                <div>
+                    <h1> ${e.Rentername}</h1>
+                    <h1>${e.Age}</h1>
+                    <h1>${e.Aadhar}</h1>
+                    <h1>${e.DL}</h1>
+                    <h1>${e.Mob}</h1>
+                    <h1>${e.Email}</h1>
+                    <h1>${e.Address}</h1>
+                    <h1>${e.CarName}</h1>
+                    <h1>${e.CarReg}</h1>
+
+
+                </div>
+                
+                `
+            })
+    
+
+            //  on click("deletee("${e.id}")")
+            // let deletee
+    
+    
     }
+
