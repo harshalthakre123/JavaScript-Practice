869cccf018db1aa8d2eb1a14bc9f9771d4fb7ee7
harshalthakre123 <harshalthakre.csaiml.oist@gmail.com>
2025-01-20 10:47:17 +0530

    crud in js

--- DB JSON/hw/data.js
+++ DB JSON/hw/data.js
@@ -1,7 +1,7 @@
 
     let getdata= async ()=>{
 
-        let url="http://localhost:3000/Renter-Details"
+        let url="http://localhost:3000/Renter-Detail"
 
         let res= await fetch(url,{method: "GET"})
         let data=await res.json()
@@ -9,34 +9,29 @@
     
     
         // CRUD read
-        let show=document.querySelector('#displayy')
+        let show=document.querySelector('#tablee')
 
         // data.map is high order function and internal function is call back function
         
         data.map((e)=>{
 
             show.innerHTML+=`
-                <div>
-                    <h1> ${e.Rentername}</h1>
-                    <h1>${e.Age}</h1>
-                    <h1>${e.Aadhar}</h1>
-                    <h1>${e.DL}</h1>
-                    <h1>${e.Mob}</h1>
-                    <h1>${e.Email}</h1>
-                    <h1>${e.Address}</h1>
-                    <h1>${e.CarName}</h1>
-                    <h1>${e.CarReg}</h1>
-
-
-                </div>
+                <tr>
+                    <td>${e.Rentername}</td>
+                    <td>${e.CarName}</td>
+                    <td>${e.Aadhar}</td>
+                    <td>${e.CarReg}</td>
+                    <td>${e.Address}</td>
+                    <td>${e.Mob}</td>
+                    <td onclick="deletee('${e.id}')"> Delete </td}
+                </tr>                
                 
                 `
             })
-    
+}
 
-            //  on click("deletee("${e.id}")")
-            // let deletee
-    
-    
+    let deletee=(id)=>{
+        let url=`http://localhost:3000/Renter-Detail/${id}`
+
+        fetch(url,{method: "DELETE"})
     }
-
